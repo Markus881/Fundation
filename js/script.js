45044ab97d526192ed674141c28a43ae8a6225da
Markus881 <ivanbogoslavets@ukr.net>
2022-09-28 20:44:15 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$('.about__items > div').on('click', function (){
+$('.about__items > div').on('click', function () {
+	$(this).children('.about-click').toggleClass('about-rotate-icon');
+	$(this).parent('.about__items').toggleClass('change-color');
 	$(this).next('.about-text').slideToggle(300);
+});
+
+
+$('.modal-overlay').on('click', function (e) {
+	if ($(e.target).closest('.modal').length == 0) {
+		$(this).fadeOut();
+	}
+});
+
+
+$('.modal__close').on('click', function () {
+	$(this).parents('.modal-overlay').fadeOut()
+})
+
+
+$('#open-modal').on('click', function () {
+	$('.modal-overlay').fadeIn()
 })
