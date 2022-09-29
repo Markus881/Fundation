6936c93a0dc68d374e3500db180b284fc1d77a33
Markus-881 <ivanbogoslavets@ukr.net>
2022-09-29 09:12:46 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ $('.about__items > div').on('click', function () {
 });
 
 
+
+
 $('.modal-overlay').on('click', function (e) {
 	if ($(e.target).closest('.modal').length == 0) {
 		$(this).fadeOut();
 	}
 });
 
-
 $('.modal__close').on('click', function () {
 	$(this).parents('.modal-overlay').fadeOut()
 })
-
 
 $('#open-modal').on('click', function () {
 	$('.modal-overlay').fadeIn()
