eb86d908de14e5a5bfc4bd8277e18e6d8e75c87a
Markus881 <ivanbogoslavets@ukr.net>
2022-10-02 19:10:11 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
+
 $('.items__row > div').on('click', function () {
 	$(this).next('.items__row-text').slideToggle(300);
 });
-
-
-
-
-
-
-
-
 
 
 $('.about__items > div').on('click', function () {
@@ -17,19 +10,39 @@ $('.about__items > div').on('click', function () {
 	$(this).next('.about-text').slideToggle(300);
 });
 
-
-
-
+// Модальне вікно(Pop-up)
 $('.modal-overlay').on('click', function (e) {
 	if ($(e.target).closest('.modal').length == 0) {
 		$(this).fadeOut();
 	}
 });
 
+
 $('.modal__close').on('click', function () {
 	$(this).parents('.modal-overlay').fadeOut()
 })
 
+
 $('#open-modal').on('click', function () {
 	$('.modal-overlay').fadeIn()
 })
+
+// Кнопка до гори
+$(window).on('scroll', function (){
+	if($(this).scrollTop() > 500){
+		$('#up').fadeIn()
+	}else{
+		$('#up').fadeOut()
+	}
+})
+
+$('#up').on('click', function () {
+	$('html, body').animate({ scrollTop: 0 }, 500)
+})
+
+// Слайдер
+$(document).ready(function(){
+	$('.slider').bxSlider({
+		controls: false
+	});
+});
